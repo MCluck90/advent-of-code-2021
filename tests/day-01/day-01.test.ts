2225a1c2dbf9025c8d9dc670ab502328c2befcb7
Mike Cluck <mike-cluck@pluralsight.com>
2021-12-01 11:09:51 -0700

    Optimize solutions

--- tests/day-01/day-01.test.ts
+++ tests/day-01/day-01.test.ts
@@ -15,78 +15,54 @@ const load = (type: 'test-1' | 'test-2' | 'puzzle') => {
 
 describe('Day 1: Sonar Sweep', () => {
   describe('Part 1', () => {
-    function countMeasurementIncreases(input: string): number {
+    function solution(input: string): number {
       const measurements = input.split('\n').map((n) => Number(n))
-      let previousMeasurement = Infinity
       let increases = 0
-      for (const measurement of measurements) {
-        if (measurement > previousMeasurement) {
-          increases++
+      for (let i = 1; i < measurements.length; i++) {
+        if (measurements[i] > measurements[i - 1]) {
+          increases += 1
         }
-        previousMeasurement = measurement
       }
       return increases
     }
 
     test('with example data', () => {
       const testData = load('test-1')
-      expect(countMeasurementIncreases(testData)).toBe(7)
+      expect(solution(testData)).toBe(7)
     })
 
     test('with puzzle input', () => {
       const testData = load('puzzle')
-      expect(countMeasurementIncreases(testData)).toBe(1529)
+      expect(solution(testData)).toBe(1529)
     })
   })
   describe('Part 2', () => {
-    function countMeasurementWindowIncreases(input: string): number {
+    function solution(input: string): number {
       const measurements = input.split('\n').map((n) => Number(n))
-
-      const rollingMeasurement = [0, 0, 0, 0]
-      const windows: number[][] = [[], [], [], []]
-      for (let i = 0; i <= measurements.length; i++) {
-        const measurement = measurements[i]
-        for (let j = 0; j < 4; j++) {
-          if (i > j - 1) {
-            if (i > 0 && (i - j) % 4 === 3) {
-              windows[j].push(rollingMeasurement[j])
-              rollingMeasurement[j] = 0
-            } else {
-              rollingMeasurement[j] += measurement
-            }
-          }
-        }
+      const windows: number[] = []
+      for (let i = 2; i < measurements.length; i++) {
+        windows.push(
+          measurements[i] + measurements[i - 1] + measurements[i - 2]
+        )
       }
 
       let increases = 0
-      for (let i = 1; ; i++) {
-        const j = i - 1
-        const row = Math.floor(i / 4)
-        const prevRow = Math.floor(j / 4)
-        const column = i % 4
-        const prevColumn = j % 4
-        const value = windows[column][row]
-        const prevValue = windows[prevColumn][prevRow]
-        if (value === undefined) {
-          break
-        }
-
-        if (value > prevValue) {
-          increases++
+      for (let i = 1; i < windows.length; i++) {
+        if (windows[i] > windows[i - 1]) {
+          increases += 1
         }
       }
-
       return increases
     }
 
     test('with example data', () => {
       const testData = load('test-2')
-      expect(countMeasurementWindowIncreases(testData)).toBe(5)
+      expect(solution(testData)).toBe(5)
     })
 
     test('with puzzle input', () => {
       const testData = load('puzzle')
-      expect(countMeasurementWindowIncreases(testData)).toBe(1567)
+      expect(solution(testData)).toBe(1567)
     })
   })
 })
